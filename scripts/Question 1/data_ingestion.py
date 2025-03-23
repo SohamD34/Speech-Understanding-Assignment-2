622eee15242f5fac50de51cd2eee700871a617e8
SohamD34 <deshmukh.2@iitj.ac.in>
2025-03-23 18:36:09 +0530

    Updated logs for data ingestion

--- scripts/Question 1/data_ingestion.py
+++ scripts/Question 1/data_ingestion.py
@@ -9,47 +9,47 @@ def download_data(url, dataset_name):
     download_url = f'https://drive.google.com/uc?id={file_id}'
     output_loc = f'data/{dataset_name}.zip'
 
-    print('Accessing the public URL...')
+    log_text('logs/question1.txt', 'Accessing the public URL...')
     gdown.download(download_url, output_loc, quiet=False)
     
-    print(f'Dataset ZIP file is downloaded at {output_loc}/file.zip')
+    log_text('logs/question1.txt', f'Dataset ZIP file is downloaded at {output_loc}/file.zip')
 
 
 
 def extract_data(data_zip_loc):
     
-    print(f'Extracting the dataset...')
+    log_text('logs/question1.txt', f'Extracting the dataset...')
     with zipfile.ZipFile(data_zip_loc, 'r') as zip_ref:
         zip_ref.extractall('data')
 
-    # os.remove(data_zip_loc)
-    print('Extraction completed!\n')
+    os.remove(data_zip_loc)
+    log_text('logs/question1.txt', 'Extraction completed!')
 
 
 
 def rename_dir(orig_dir_path, new_dir_name):
     
-    print(f'Renaming the directory...')
+    log_text('logs/question1.txt', 'Renaming the directory...')
     os.rename(orig_dir_path, new_dir_name)
-    print(f'Directory renamed to {new_dir_name}\n')
+    log_text('logs/question1.txt', f'Directory renamed to {new_dir_name}\n')
 
 
 
 if __name__ == '__main__':
 
-    print('Ingesting Vox Celeb 1 Dataset...\n')
+    log_text('logs/question1.txt', 'Ingesting Vox Celeb 1 Dataset...\n')
     url = 'https://drive.google.com/file/d/1AC-Q8dEw1LTPdpEi5ofS04ZmZWdl8BBg/view?usp=drive_link'
     dataset_name = 'voxceleb1'
 
-    # download_data(url, dataset_name)
-    # extract_data('data/voxceleb1.zip')
+    download_data(url, dataset_name)
+    extract_data('data/voxceleb1.zip')
     rename_dir('data/wav', 'data/voxceleb1')
 
-    print('Ingesting Vox Celeb 2 Dataset...\n')
+    log_text('logs/question1.txt', 'Ingesting Vox Celeb 2 Dataset...\n')
     url = 'https://drive.google.com/file/d/1Onu4jzcyasrxTR1rRT9rl9AfkUrueM6o/view?usp=drive_link'
     dataset_name = 'voxceleb2'
 
-    # download_data(url, dataset_name)
+    download_data(url, dataset_name)
     extract_data('data/voxceleb2.zip')
     rename_dir('data/aac', 'data/voxceleb2')
     
--- scripts/Question 1/utils.py
+++ scripts/Question 1/utils.py
@@ -7,12 +7,13 @@ def setup_logger(log_file_path):
     logger = logging.getLogger('custom_logger')
     logger.setLevel(logging.INFO)
     
-    fh = logging.FileHandler(log_file_path)
-    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    if not logger.handlers:
+        fh = logging.FileHandler(log_file_path)
+        formatter = logging.Formatter('%(asctime)s - %(message)s')
 
-    fh.setLevel(logging.INFO)    
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+        fh.setLevel(logging.INFO)    
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
     
     return logger
 
